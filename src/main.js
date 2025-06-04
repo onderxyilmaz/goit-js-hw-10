85ffc388bf0ff9fc9cfb0deabe1c42031bd38c1f
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-06-04 23:04:51 +0300

    Convert project to Vanilla JavaScript

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './index.css'
+
+// Ana sayfa için basit bir JavaScript kodu
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Homework 10 - Main Page Loaded')
+})
